7c99d256c9c77eb020bbac2050572fa0fd68186c
Android Studio <113397505+optimalcoding@users.noreply.github.com>
2026-03-12 23:39:02 -0400

    Update aimassist.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/aimassist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/aimassist.java
@@ -21,13 +21,17 @@ public class aimassist {
     private DcMotorEx aim;
     private GoBildaPinpointDriver pinpoint;
 
+    private final double TICKS_PER_DEGREE = 1.0655;
+
 
     private double kP = 0.01;
 
-    private double kD = 0.0000;
+    private double kD = 0.0001;
 
-    double goalX = 138;
-    double goalY = 138;
+    private final double MIN_TURRET_ANGLE = -150.0;
+    private final double MAX_TURRET_ANGLE = 150.0;
+
+
 
     private double lasterror = 0;
 
@@ -40,17 +44,15 @@ public class aimassist {
     private final ElapsedTime timer = new ElapsedTime();
 
     public void init(HardwareMap hwMap) {
-        
-        aim = hwMap.get(DcMotorEx.class, "aim");
-        
-        
-        pinpoint = hwMap.get(GoBildaPinpointDriver.class, "pinpoint");
-        pinpoint.setOffsets(0,-9.5, DistanceUnit.INCH);
-        pinpoint.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
-        pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
-        pinpoint.resetPosAndIMU();
 
+        aim = hwMap.get(DcMotorEx.class, "aim");
+
+        aim.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         aim.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        aim.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+
     }
 
     public void setkP(double newkP) {
@@ -73,33 +75,38 @@ public class aimassist {
         timer.reset();
     }
 
-   
-    
+
+
     public void update(Pose2D pos, double goalX, double goalY) {
-        
+
         double deltaTime = timer.seconds();
         timer.reset();
-        
+        if (deltaTime <= 0) deltaTime = 0.001;
+
         double dx = goalX - pos.getX(DistanceUnit.INCH);
         double dy = goalY - pos.getY(DistanceUnit.INCH);
         double robotHeading = pos.getHeading(AngleUnit.DEGREES);
 
-        double targetAngle = Math.toDegrees(Math.atan2(dy, dx));
+        double targetAngleField = Math.toDegrees(Math.atan2(dy, dx));
+        double targetTurretRelative = AngleUnit.normalizeDegrees(targetAngleField - robotHeading);
+        double currentTurretAngle = aim.getCurrentPosition() / TICKS_PER_DEGREE;
 
 
-        double error = AngleUnit.normalizeDegrees(targetAngle - robotHeading);
+        double error = AngleUnit.normalizeDegrees(targetTurretRelative - currentTurretAngle);
+
+
         double pTerm = error * kP;
+        double dTerm = ((error - lasterror) / deltaTime) * kD;
+        double power = Range.clip(pTerm + dTerm, -0.8, 0.8);
 
 
-         double dTerm = (deltaTime > 0) ? (error - lasterror) * kD : 0;
-         if (Math.abs(error) < goalTolerance) {
-            power = 0;
-                } else {
-                    power = Range.clip(pTerm + dTerm, -maxPower, maxPower);
-                }
-        
+        dTerm = (deltaTime > 0) ? (error - lasterror) * kD : 0;
+        if (Math.abs(error) < 1.5) { // 1.5 degree tolerance
+            aim.setPower(0);
+        } else {
+            aim.setPower(power);
+        }
 
-        aim.setPower(power);
         lasterror = error;
 
     }
