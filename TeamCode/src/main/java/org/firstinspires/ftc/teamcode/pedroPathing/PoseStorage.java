116776ef09b4edb3c18b2afe2d1a385cf9f4ddf7
Android Studio <113397505+optimalcoding@users.noreply.github.com>
2026-03-13 00:58:13 -0400

    Create PoseStorage.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/PoseStorage.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.pedropathing.geometry.Pose;
+
+public class PoseStorage {
+    // This variable will hold the position between Auto and TeleOp
+    public static Pose currentPose = new Pose(0, 0, 0);
+}
